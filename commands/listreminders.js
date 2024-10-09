3d8e4e88aa201e6241dafd98e0d6879a39dc1846
Colton B <main@coltvel.com>
2024-10-09 03:16:59 -0400

    Finished page array codes, will tune up with content in the pages later.

--- commands/listreminders.js
+++ commands/listreminders.js
@@ -3,6 +3,14 @@ const { listAllRows } = require(`../events/databaseManager.js`);
 const { decryptData } = require('../libs/encryption.js');
 const embedPage = require('../libs/embedPage.js');
 
+async function pageArray(array, page) {
+    const itemsPerPage = 5;
+
+    const startIndex = (page - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return array.slice(startIndex, endIndex);
+}
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('listreminders')
@@ -11,7 +19,6 @@ module.exports = {
         const embeds = [];
         var list = [];
 
-        // Decrypt test
         var listRows = await listAllRows();
         var JSONRow = JSON.stringify(listRows, null, 2);
         var parsedJSON = JSON.parse(JSONRow);
@@ -19,8 +26,11 @@ module.exports = {
             list.push(decryptData(parsedJSON[key].description));
         }
 
-        for (var i = 0; i < list.length; i++) {
-            embeds.push(new EmbedBuilder().setDescription(`Snitch/Pearl Name: ${list[i]}`).setColor("Blurple"));
+        for (var i = 0; i < Math.ceil(list.length / 5); i++) {
+            const pageItems = await pageArray(list, i + 1);
+            embeds.push(new EmbedBuilder()
+                .setTitle('Snitch/Pearl Name List')
+                .setDescription(pageItems.join('\n')).setColor("Blurple"));
         }
 
         await embedPage(interaction, embeds)
